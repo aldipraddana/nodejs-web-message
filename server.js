86705f120e71e84dceb471133e8aad3cddad81a2
aldipraddana <aldipraddan>
2021-01-06 04:18:26 -0800

    add bootstrap aand jqery

--- server.js
+++ server.js
@@ -4,9 +4,18 @@ let session = require('express-session');
 let server = require('http').createServer(app);
 let io = require('socket.io').listen(server);
 const mysql = require('mysql');
+const path = require('path');
+
+// prepare server
+// app.use('/api', api); // redirect API calls
+// app.use('/', express.static(__dirname + '/www')); // redirect root
+app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js')); // redirect bootstrap JS
+app.use('/js', express.static(__dirname + '/node_modules/jquery/dist')); // redirect JS jQuery
+app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css')); // redirect CSS bootstrap
 
 app.use(express.static('public'));
 app.use(express.urlencoded({extended: false}));
+
 app.use(session({
 	secret: 'secret',
 	resave: true,
