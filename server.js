1b5c5daaf8fc8138a0269269f8d4ff04b558f794
aldipraddana <aldipraddan>
2021-01-10 04:37:50 -0800

    ubah tampilan login

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const path = require('path');
 
 // prepare server
 // app.use('/api', api); // redirect API calls
-// app.use('/', express.static(__dirname + '/www')); // redirect root
+app.use('/custom', express.static(__dirname + '/node_modules/custom/')); // redirect root
 app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js')); // redirect bootstrap JS
 app.use('/js', express.static(__dirname + '/node_modules/jquery/dist')); // redirect JS jQuery
 app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css')); // redirect CSS bootstrap
